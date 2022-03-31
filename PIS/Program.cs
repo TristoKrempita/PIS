418c5772bd8efbca46614a0d8e5733329920f693
nbilic <72692113+nbilic@users.noreply.github.com>
2022-03-31 15:42:49 +0200

    Added extra novo
    Novo novo

--- PIS/Program.cs
+++ PIS/Program.cs
@@ -11,7 +11,7 @@ namespace PIS_1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World");
-            Console.WriteLine("Novo");
+            Console.WriteLine("Novo novo");
         }
     }
 }
